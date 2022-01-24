a6bfbc8159927e442f2a785c1b484e35d775c3b2
Mario van Zeist <m.vanzeist@home.nl>
2022-01-24 17:38:14 +0100

    Cleanup (Remove non used library)

--- BlazorConstraint/SimpleData.cs
+++ BlazorConstraint/SimpleData.cs
@@ -2,7 +2,7 @@
 {
     public class SimpleData : IMyInterface
     {
-        public static IEnumerable<SimpleData> List = new[] { new SimpleData { Name = "1" }, new SimpleData { Name = "2" } };
+        public static readonly IEnumerable<SimpleData> List = new[] { new SimpleData { Name = "1" }, new SimpleData { Name = "2" } };
         public string Name { get; set; } = default!;
     }
 }
